9f4974908bba0e13eb9f2f2e35582e1c8ced4c19
yusuke-sudou <sudoukky22@gmail.com>
2024-01-13 15:34:42 +0900

    スッキリ３章for文①

---
+++ src/chapter3/For1.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 0; i < 10; i++) {
+			System.out.println("こんにちは");
+		}
+
+	}
+
+}
