aff4c57b1e68b251038f318c530a09ba75b8c724
yusuke-sudou <sudoukky22@gmail.com>
2024-01-13 15:51:18 +0900

    スッキリ３章for文②

---
+++ src/chapter3/For2.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 0; i < 3; i++) {
+			System.out.print("現在" + (i + 1) +"週目→");
+		}
+
+	}
+
+}
