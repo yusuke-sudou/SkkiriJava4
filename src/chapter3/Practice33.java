f3bd69201c0b5a74c551b577bcb03aa75540f371
yusuke-sudou <sudoukky22@gmail.com>
2024-01-14 16:26:35 +0900

    練習問題3-3（正式回答）

---
+++ src/chapter3/Practice33.java
@@ -0,0 +1,23 @@
+package chapter3;
+
+public class Practice33 {
+
+	public static void main(String[] args) {
+		int isHungry = 1;
+	    String food = "おにぎり";
+	    System.out.println("こんにちは");
+	    if (isHungry == 0) {
+	      System.out.println("お腹がいっぱいです");
+	    } else {
+	      System.out.println("はらぺこです");
+	    }
+	    if (isHungry == 1) {
+	      System.out.println(food + "をいただきます");
+	    }
+	  System.out.println("ごちそうさまでした");
+	  }
+		
+
+	}
+
+
