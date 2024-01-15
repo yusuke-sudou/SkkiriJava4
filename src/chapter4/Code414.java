a92bc0b03a692d8316370b49cb09695d88f5592d
yusuke-sudou <sudoukky22@gmail.com>
2024-01-15 16:39:14 +0900

    拡張for文

---
+++ src/chapter4/Code414.java
@@ -0,0 +1,15 @@
+package chapter4;
+
+public class Code414 {
+
+	public static void main(String[] args) {
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int value : scores) {
+			System.out.println(value);
+		}
+
+	}
+
+}
+// 拡張for文で配列を回す
+// for (要素の方　任意の変数　：　配列変数名) {
