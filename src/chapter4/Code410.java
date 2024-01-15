95cfbc8dba823fb9ecf6879776b111ac3c1fc75f
yusuke-sudou <sudoukky22@gmail.com>
2024-01-15 15:56:31 +0900

    カウント集計

--- src/chapter4/Code410.java
+++ src/chapter4/Code410.java
@@ -5,8 +5,10 @@ public class Code410 {
 	public static void main(String[] args) {
 		int[] scores = {20, 30, 40, 50, 80};
 		int sum = 0;
+		// 集計合計を入れるための変数を初期化して準備
 		for (int i = 0; i < scores.length; i++) {
 			sum += scores[i];
+			// 1科目ずつ変数sumに合算する
 		}
 		int avg = sum / scores.length;
 		System.out.println("合計点：" + sum);
