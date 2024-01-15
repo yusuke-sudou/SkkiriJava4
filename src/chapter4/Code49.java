5521ff59564115311728425232c612fcfb952e1a
yusuke-sudou <sudoukky22@gmail.com>
2024-01-15 14:39:55 +0900

    for文を使って配列を扱う

---
+++ src/chapter4/Code49.java
@@ -0,0 +1,13 @@
+package chapter4;
+
+public class Code49 {
+
+	public static void main(String[] args) {
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int i = 0; i < scores.length; i++) {
+			System.out.println(scores[i]);
+		}
+
+	}
+
+}
