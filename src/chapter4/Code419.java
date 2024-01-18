8302b05f82c797e9c00aa9a1e29942280de20724
yusuke-sudou <sudoukky22@gmail.com>
2024-01-18 16:54:30 +0900

    親配列と子配列の要素数を表示

---
+++ src/chapter4/Code419.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+public class Code419 {
+
+	public static void main(String[] args) {
+		int[][] scores = {{40, 50, 60}, {80, 60, 70}};
+		// 2次元配列の初期化
+		System.out.println(scores.length);
+		// 2が出力される
+		System.out.println(scores[0].length);
+		// 3が出力される
+	}
+
+}
