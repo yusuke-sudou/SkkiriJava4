43ec9f275a3294d9ad692d6b3fabf592f4072c03
yusuke-sudou <sudoukky22@gmail.com>
2024-01-18 21:35:25 +0900

    テスト1

---
+++ src/taisaku/Test1.java
@@ -0,0 +1,17 @@
+package taisaku;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		for (int i = 2; i <= 10; i += 2) {
+			System.out.println(i);
+		}
+	}
+
+}
+
+// int i = 2; で2からスタートするようにする
+
+// i <= 10;  で2～10という枠に納める　（※＜を使用すると10が含まれなくなるので　<=を使用する）
+
+// i += 2 　で2つずつ足していくかたちになる
