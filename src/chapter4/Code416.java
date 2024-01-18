95f520c7b8a82ef835ea621e2fe4b66e9dcd6ce0
yusuke-sudou <sudoukky22@gmail.com>
2024-01-18 15:12:02 +0900

    ガベージコレクション

---
+++ src/chapter4/Code416.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+public class Code416 {
+
+	public static void main(String[] args) {
+		boolean judge = true;
+		if (judge == true) {
+			int[] array = {1, 2, 3};
+		}
+
+	}
+
+}
+ // ガベージコレクションの説明用
