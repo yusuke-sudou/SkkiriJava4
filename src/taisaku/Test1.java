82bfceaf43a433c99a4e8506d018ee1cc8e1c71c
yusuke-sudou <sudoukky22@gmail.com>
2024-01-19 09:16:07 +0900

    テスト再確認

--- src/taisaku/Test1.java
+++ src/taisaku/Test1.java
@@ -3,7 +3,7 @@ package taisaku;
 public class Test1 {
 
 	public static void main(String[] args) {
-		for (int i = 2; i <= 10; i += 2) {
+		for (int i = 2; i <= 10; i+=2) {
 			System.out.println(i);
 		}
 	}
--- src/taisaku/Test2.java
+++ src/taisaku/Test2.java
@@ -5,13 +5,12 @@ public class Test2 {
 	public static void main(String[] args) {
 		int count = 0;
 		int sum = 0;
-		for (int i = 2; i <= 10; i += 2) {
+		for (int i = 2; i <= 10; i+=2) {
 			count++;
-			sum += i;
+			sum +=i;
 		}
 		System.out.println("偶数の数：" + count);
 		System.out.println("偶数の合計：" + sum);
-
 	}
 
 }
