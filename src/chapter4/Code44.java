3adfd561f0469997dd782fa4a3d8cc0c0526b5b7
yusuke-sudou <sudoukky22@gmail.com>
2024-01-20 11:38:39 +0900

    配列復習②

---
+++ src/chapter4/Code44.java
@@ -0,0 +1,16 @@
+package chapter4;
+
+public class Code44 {
+
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+		int num = scores.length;
+		System.out.println("要素の数：" + num);
+
+	}
+
+}
+
+// 配列の長さを調べる
+
+// 配列変数名.length
