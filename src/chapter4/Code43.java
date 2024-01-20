21384428cca4b552d88578f08e68b3702928edc2
yusuke-sudou <sudoukky22@gmail.com>
2024-01-20 11:34:42 +0900

    配列復習①

---
+++ src/chapter4/Code43.java
@@ -0,0 +1,15 @@
+package chapter4;
+
+public class Code43 {
+
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+
+	}
+
+}
+
+// ①int型の要素を代入できる配列変数scoresを用意（[]が必要）
+// ②int型の要素を5つ作成してscoresに代入し、配列scoresの完成
+
+// 上記二つを同時に行えるのがこの式
---
+++ src/chapter5/Code51.java
@@ -0,0 +1,10 @@
+package chapter5;
+
+public class Code51 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
