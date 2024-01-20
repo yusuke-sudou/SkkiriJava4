41367e4b5615682ed48487bc49b0cc41c673e8df
yusuke-sudou <sudoukky22@gmail.com>
2024-01-20 14:02:00 +0900

    配列復習⑦

--- src/chapter4/Code414.java
+++ src/chapter4/Code414.java
@@ -4,12 +4,16 @@ public class Code414 {
 
 	public static void main(String[] args) {
 		int[] scores = {20, 30, 40, 50, 80};
-		for (int value : scores) {
-			System.out.println(value);
+		for (int num : scores) {
+			System.out.println(num);
 		}
 
 	}
 
 }
 // 拡張for文で配列を回す
-// for (要素の方　任意の変数　：　配列変数名) {
+
+// for (要素の型　任意の変数　：　配列変数名) {
+// for (int num : scores) {
+
+// ループ変数や配列の添え字を記述する必要がなくなる
