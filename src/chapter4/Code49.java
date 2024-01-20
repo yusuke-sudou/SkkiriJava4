da33108c7f28199584480f91c55cc2f9861df5de
yusuke-sudou <sudoukky22@gmail.com>
2024-01-20 12:05:17 +0900

    配列復習⑤

--- src/chapter4/Code49.java
+++ src/chapter4/Code49.java
@@ -6,6 +6,7 @@ public class Code49 {
 		int[] scores = {20, 30, 40, 50, 80};
 		for (int i = 0; i < scores.length; i++) {
 			System.out.println(scores[i]);
+			// scores[i]でループのたびにiの値が0～4で変化する
 		}
 
 	}
