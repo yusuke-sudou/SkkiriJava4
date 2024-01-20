9a3c218cf47ea2dcb6b11c8c378a0aa66a5ca062
yusuke-sudou <sudoukky22@gmail.com>
2024-01-20 11:44:32 +0900

    配列復習③

---
+++ src/chapter4/Code45.java
@@ -0,0 +1,15 @@
+package chapter4;
+
+public class Code45 {
+
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+		scores[1] = 30;
+		// 2つ目の要素scores[1]に代入
+		System.out.println(scores[1]);
+		// 2つ目の要素scores[1]の中身を表示
+
+	}
+
+}
+
