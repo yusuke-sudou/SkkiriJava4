217939761ada3ade536523f82afbac63d37d9563
yusuke-sudou <sudoukky22@gmail.com>
2024-01-20 13:47:34 +0900

    配列復習⑥

--- src/chapter4/Code411.java
+++ src/chapter4/Code411.java
@@ -7,12 +7,14 @@ public class Code411 {
 		int count = 0;
 		for (int i = 0; i < scores.length; i++) {
 			if (scores[i] >= 50) {
-				count++;
+				count++;			
 			}
 		}
-		System.out.println("50点以上の科目の数は" + count);
+		System.out.println("50点以上の科目の数は：" + count);
 	}
 
 }
+
+// ある条件と一致する要素の数を数えるカウント集計という処理
 
 // if(scores[i] >= 50)とcount++にて、条件に合致する要素があればカウントする
