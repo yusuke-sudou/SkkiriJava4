c54a6bf7d1df83237d3cb38cb2bb578145dc6f89
yusuke-sudou <sudoukky22@gmail.com>
2024-01-20 21:26:45 +0900

    メソッドについて

--- src/chapter5/Code51.java
+++ src/chapter5/Code51.java
@@ -3,8 +3,25 @@ package chapter5;
 public class Code51 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("メソッドを呼び出します");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました");
 
+	}
+	
+	public static void hello() {
+		System.out.println("須藤さん、こんにちは");
 	}
 
 }
+
+////////// メソッドの定義 ////////////
+
+// public static void メソッド名() {
+//   メソッドが呼び出されたときの処理
+// }
+
+////////// メソッドの呼び出し /////////
+
+// メソッド名（）
+
