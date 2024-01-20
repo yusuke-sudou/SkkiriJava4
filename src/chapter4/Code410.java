142cd679b1c554919ecd7577f1a30b7857f3bce5
yusuke-sudou <sudoukky22@gmail.com>
2024-01-20 13:37:55 +0900

    配列復習⑥

--- src/chapter4/Code410.java
+++ src/chapter4/Code410.java
@@ -3,14 +3,15 @@ package chapter4;
 public class Code410 {
 
 	public static void main(String[] args) {
-		int[] scores = {20, 30, 40, 50, 80} ;
+		int[] scores = {20, 30, 40, 50, 80};
 		int sum = 0;
 		for (int i = 0; i < scores.length; i++) {
 			sum += scores[i];
 		}
 		int avg = sum / scores.length;
-		System.out.println("合計点：" + sum);
-		System.out.println("平均点：" + avg);
+		// 平均を算出
+		System.out.println("合計点" + sum);
+		System.out.println("平均点" + avg);
 	}
 
 		
