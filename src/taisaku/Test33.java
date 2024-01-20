11398aebdaa359798f8e5b2d10f25818174e33ba
yusuke-sudou <sudoukky22@gmail.com>
2024-01-20 16:48:40 +0900

    テスト対策3問目

---
+++ src/taisaku/Test33.java
@@ -0,0 +1,16 @@
+package taisaku;
+
+public class Test33 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			if (i < 10) {
+				System.out.print(i + ",");
+			} else {
+				System.out.print(i);
+			}
+		}
+
+	}
+
+}
