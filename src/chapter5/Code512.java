df29edfe696c7471df5ed4be27eebe8fccbe483d
yusuke-sudou <sudoukky22@gmail.com>
2024-01-31 10:41:12 +0900

    引数に配列を受け取るメソッド

---
+++ src/chapter5/Code512.java
@@ -0,0 +1,19 @@
+package chapter5;
+
+public class Code512 {
+	
+	public static void printArray(int[] array) {
+		for(int element : array) {
+			System.out.println(element);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3};
+		printArray(array);
+		// 配列を渡す
+
+	}
+
+}
+// (int[] array)の部分にて引数に配列型を指定
