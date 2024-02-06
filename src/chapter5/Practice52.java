889f57c271fd0d2210dbe5069154a40678153969
yusuke-sudou <sudoukky22@gmail.com>
2024-02-06 10:37:37 +0900

    ５章練習5-3　オーバーライド

--- src/chapter5/Practice52.java
+++ src/chapter5/Practice52.java
@@ -3,11 +3,16 @@ package chapter5;
 public class Practice52 {
 
 	public static void main(String[] args) {
-		String title = "お誘い";
 		String address = "yamada@taro";
 		String text = "今度、飲みに行きませんか";
-		email(title, address, text);
+		email(address, text);
 
+	}
+	
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールにを送信しました");
+		System.out.println("件名：無題");
+		System.out.println("本文：" + text);
 	}
 	
 	public static void email(String title, String address, String text) {
