d4d06d1342700704e07cc8ca9280ee441d3ac5d8
yusuke-sudou <sudoukky22@gmail.com>
2024-02-06 10:57:00 +0900

    ５章練習問題5-4

---
+++ src/chapter5/Practice54.java
@@ -0,0 +1,25 @@
+package chapter5;
+
+public class Practice54 {
+
+	public static void main(String[] args) {
+		double triangleArea = calcTriangleArea(10.0, 5.0);
+		System.out.println("三角形の面積：" + triangleArea + "平方cm");
+		double circleArea = calcCircleArea(5.0);
+		System.out.println("円の面積：" + circleArea + "平方cm");
+
+	}
+	
+	public static double calcTriangleArea(double bottom, double height) {
+		double area = (bottom * height) / 2;
+		return area;
+		
+		// 上記２つは1つにまとめて「return (bottom * height) / 2;」とすることも可能
+	}
+	
+	public static double calcCircleArea(double radius) {
+		double area = radius * radius * 3.14;
+		return area;
+	}
+
+}
