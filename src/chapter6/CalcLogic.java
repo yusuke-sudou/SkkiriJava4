e4e953a65a058c71ad6cbdb199cd00a138b01ffa
yusuke-sudou <sudoukky22@gmail.com>
2024-02-11 10:16:23 +0900

    計算機プログラムを分割

---
+++ src/chapter6/CalcLogic.java
@@ -0,0 +1,14 @@
+package chapter6;
+
+public class CalcLogic {
+
+	public static int tasu(int a, int b) {
+		return(a + b);
+
+	}
+	
+	public static int hiku(int a, int b) {
+		return(a - b);
+	}
+
+}
