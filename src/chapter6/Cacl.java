8189e34f4a18bb78e7408f19353f504cf2fbc019
yusuke-sudou <sudoukky22@gmail.com>
2024-02-12 10:41:40 +0900

    コメント追加

--- src/chapter6/Cacl.java
+++ src/chapter6/Cacl.java
@@ -5,6 +5,7 @@ public class Cacl {
 	public static void main(String[] args) {
 		int a = 10; int b = 2;
 		int total = CalcLogic.tasu(a, b);
+		// ドット（.）は日本語で「～の」という意味
 		int delta = CalcLogic.hiku(a, b);
 		System.out.println("足すと" + total + "、引くと" + delta);
 
