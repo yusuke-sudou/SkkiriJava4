469a97d79491524c46fe46153ee7342b8e0b40e1
yusuke-sudou <sudoukky22@gmail.com>
2024-02-25 08:41:16 +0900

    操作の宣言方法（HEROクラス眠る操作）

--- src/chapter7/Hero.java
+++ src/chapter7/Hero.java
@@ -4,6 +4,12 @@ public class Hero {
 	String name; //名前の宣言
 	int hp; //HPの宣言
 
+	// 眠る操作
+	public void sleep() {
+		this.hp = 100; //自分自身のhpフィールド
+		System.out.println(this.name + "は、眠って回復した！"); // this.nameは自分自身のnameフィールド
+	}
+	
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
--- src/chapter7/Matango.java
+++ src/chapter7/Matango.java
@@ -2,7 +2,7 @@ package chapter7;
 
 public class Matango {
 	int hp;
-	int level = 10; //お化けキノコのレベルに初期値10を設定
+	final int LEVEL = 10; //お化けキノコのレベルに初期値10を設定
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
@@ -10,3 +10,5 @@ public class Matango {
 	}
 
 }
+
+// フィールドの先頭にfinalを付けると値を書き換えられない定数フィールドになる
