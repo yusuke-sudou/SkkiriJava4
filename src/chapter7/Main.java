9d02e3fac7b6e72ca6697fc9349a30dba0353771
yusuke-sudou <sudoukky22@gmail.com>
2024-02-25 09:22:32 +0900

    仮想世界に勇者を生み出すプログラム

--- src/chapter7/Main.java
+++ src/chapter7/Main.java
@@ -11,6 +11,11 @@ public class Main {
 		h.hp = 100; // 変数hのhpに代入
 		System.out.println("勇者" + h.name + "を生み出しました！");
 		
+		// 3.勇者のメソッドを呼び出してゆく
+		h.sit(5); // 5秒座れ
+		h.slip(); // 転べ
+		h.sit(25); // 25秒座れ
+		h.run(); // 逃げろ
 	}
 
 }
