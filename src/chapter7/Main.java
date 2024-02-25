5a3fe67b7226581712326fc9110d4d8b46a757fc
yusuke-sudou <sudoukky22@gmail.com>
2024-02-25 09:40:55 +0900

    仮想世界に勇者とお化けキノコ２匹を生み出す

--- src/chapter7/Main.java
+++ src/chapter7/Main.java
@@ -9,13 +9,27 @@ public class Main {
 		// 2.フィールドに初期値をセット
 		h.name = "ミナト"; // 変数ｈのnameに代入
 		h.hp = 100; // 変数hのhpに代入
-		System.out.println("勇者" + h.name + "を生み出しました！");
+		// System.out.println("勇者" + h.name + "を生み出しました！");
 		
 		// 3.勇者のメソッドを呼び出してゆく
-		h.sit(5); // 5秒座れ
-		h.slip(); // 転べ
-		h.sit(25); // 25秒座れ
-		h.run(); // 逃げろ
+		// h.sit(5); // 5秒座れ
+		// h.slip(); // 転べ
+		// h.sit(25); // 25秒座れ
+		// h.run(); // 逃げろ
+		
+		Matango m1 = new Matango(); // お化けキノコA（1匹目）生成し初期化
+		m1.hp = 50;
+		m1.suffix = 'A';
+		
+		Matango m2 = new Matango(); // お化けキノコB（2匹目）生成し初期化
+		m2.hp = 48;
+		m2.suffix = 'B';
+		
+		// 冒険の始まり
+		h.slip(); // 勇者転ぶ
+		m1.run(); // お化けキノコA逃げる
+		m2.run(); // お化けキノコB逃げる
+		h.run(); // 勇者も逃げる
 	}
 
 }
