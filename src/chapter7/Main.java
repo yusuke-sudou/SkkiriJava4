4aa64ead6365ffc833163ec34af49de58ee98ad8
yusuke-sudou <sudoukky22@gmail.com>
2024-02-25 09:18:00 +0900

    勇者インスタンスを生成

--- src/chapter7/Main.java
+++ src/chapter7/Main.java
@@ -3,12 +3,14 @@ package chapter7;
 public class Main {
 
 	public static void main(String[] args) {
-		// (以下の内容をJavaで記述していく)
-		// 勇者よ、この仮想世界に生まれよ！
-		// お化けキノコよ、この仮想世界に生まれよ！
-		// 勇者よ、戦え！
-		// お化けキノコよ、逃げろ！
-
+		// 1.勇者を生成
+		Hero h = new Hero(); // Heroクラスからインスタンスを生成し、変数ｈに入れる
+		
+		// 2.フィールドに初期値をセット
+		h.name = "ミナト"; // 変数ｈのnameに代入
+		h.hp = 100; // 変数hのhpに代入
+		System.out.println("勇者" + h.name + "を生み出しました！");
+		
 	}
 
 }
