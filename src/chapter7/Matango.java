a72cfdbdcc8eae953d9b4f49d4e3d0d54cc366a1
yusuke-sudou <sudoukky22@gmail.com>
2024-02-25 09:32:14 +0900

    お化けキノコのクラス定義

--- src/chapter7/Matango.java
+++ src/chapter7/Matango.java
@@ -3,10 +3,9 @@ package chapter7;
 public class Matango {
 	int hp;
 	final int LEVEL = 10; //お化けキノコのレベルに初期値10を設定
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	char suffix;
+	public void run() {
+		System.out.println("お化けキノコ" + this.suffix + "は逃げ出した！");
 	}
 
 }
