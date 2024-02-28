bb4b851ae7c6d3f90060df3b8756013c4263b7ad
yusuke-sudou <sudoukky22@gmail.com>
2024-02-28 10:35:17 +0900

    剣を装備した勇者を生み出す

--- src/chapter9/Main.java
+++ src/chapter9/Main.java
@@ -5,14 +5,14 @@ package chapter9;
 public class Main {
 
 	public static void main(String[] args) {
-		Hero h1;
-		h1 = new Hero();
-		h1.hp = 100;
-		Hero h2;
-		h2 = h1;
-		h2.hp = 200;
-		System.out.println(h1.hp);
-
+		Sword s = new Sword();
+		s.name = "炎の剣";
+		s.damage = 10;
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+		h.sword = s;
+		System.out.println("現在の武器は" + h.sword.name);
 	}
 
 }
