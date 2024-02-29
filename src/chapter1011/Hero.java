c022aff30f6e443bf10f8e8d840f28d5b1a271b5
yusuke-sudou <sudoukky22@gmail.com>
2024-02-29 14:42:21 +0900

    slip()はオーバーライドできないHeroクラス

--- src/chapter1011/Hero.java
+++ src/chapter1011/Hero.java
@@ -1,17 +1,15 @@
 package chapter1011;
 
-import chapter7.Matango;
-
 public class Hero {
 
 	String name = "ミナト";
 	int hp = 100;
 	
-	// 攻撃
-	public void attack(Matango m) {
-		System.out.println(this.name + "の攻撃！");
-		m.hp -= 5;
-		System.out.println("5ポイントのダメージをあたえた！");
+	// 転ぶ
+	public final void slip() {
+		this.hp -= 5;
+		System.out.println(this.name + "は転んだ！");
+		System.out.println("5のダメージ");
 	}
 	// 逃げる
 	public void run() {
